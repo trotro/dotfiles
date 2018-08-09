7d5ba60bed35218ddf4887d60714be68b970625d
Nicolas Trauwaen <nicolas.trauwaen@cdbdx.biz>
2018-08-09 13:34:58 +0200

    ajout alias fish shell

---
+++ alias.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function odc --description alias\ odc\ cd\ /mnt/c/Users/nicolas.trauwaen/OneDrive\\\ -\\\ Cdiscount/
+	cd /mnt/c/Users/nicolas.trauwaen/OneDrive\ -\ Cdiscount/ $argv;
+end
