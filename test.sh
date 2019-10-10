756d52652a8f013347ac0016cb9013cebd1234ed
nicolas Trauwaen <ntrauwaen@gmail.com>
2019-10-10 15:20:17 +0200

    maj test

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
 (
 # find all executables and run `shellcheck`
-for f in $(find . -type f -executable); do
-	shellcheck $f && echo -e "---\nSucessfully linted $f\n---"
-done
-) || true
+#for f in $(find . -type f -not -path '*.git*' | sort -u); do
+#	shellcheck $f && echo -e "---\nSucessfully linted $f\n---"
+#done
+#) || true
+find . -type f -not -path '*.git*' -exec shellcheck "{}" \; -exec echo "[OK]: sucessfully linted $f" \;
