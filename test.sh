a54e848e28596dc2d5fd22ed1a1dc5f91165ad04
nicolas Trauwaen <ntrauwaen@gmail.com>
2019-10-10 15:46:40 +0200

    maj test

--- test.sh
+++ test.sh
@@ -2,10 +2,26 @@
 set -e
 set -o pipefail
 
-(
+#one liner but fails during test
+#find . -type f -not -path '*.git*' -exec shellcheck "{}" \; -exec echo "[OK]: sucessfully linted $f" \;
+ERRORS=()
+
 # find all executables and run `shellcheck`
-#for f in $(find . -type f -not -path '*.git*' | sort -u); do
-#	shellcheck $f && echo -e "---\nSucessfully linted $f\n---"
-#done
-#) || true
-find . -type f -not -path '*.git*' -exec shellcheck "{}" \; -exec echo "[OK]: sucessfully linted $f" \;
+for f in $(find . -type f -not -path '*.git*' -not -name "yubitouch.sh" | sort -u); do
+	if file "$f" | grep --quiet shell; then
+		{
+			shellcheck "$f" && echo "[OK]: successfully linted $f"
+		} || {
+			# add to errors
+			ERRORS+=("$f")
+		}
+	fi
+done
+
+if [ ${#ERRORS[@]} -eq 0 ]; then
+	echo "No errors, hooray"
+else
+	echo "These files failed shellcheck: ${ERRORS[*]}"
+	exit 1
+fi
+#EOF
