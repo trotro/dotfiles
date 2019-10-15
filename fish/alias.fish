37c446292222e1bf3a8d3d82632e7887607ad7a6
nicolas Trauwaen <ntrauwaen@gmail.com>
2019-10-15 11:27:37 +0200

    retrait doublon

--- fish/alias.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function odc --description alias\ odc\ cd\ /mnt/c/Users/nicolas.trauwaen/OneDrive\\\ -\\\ Cdiscount/
-	cd /mnt/c/Users/nicolas.trauwaen/OneDrive\ -\ Cdiscount/ $argv;
-end
