4d7da2dd0aa7cb385911d403678823e78cfba1ae
nicolas Trauwaen <ntrauwaen@gmail.com>
2019-10-25 14:47:43 +0200

    maj fonctions du & plantuml

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,10 +1,20 @@
+#!/usr/local/bin/fish
 #aliases
+set os (uname)
+
 function difu --wraps diff --description 'alias difu=diff -u --color'
   diff -u --color $argv
 end
-function du1 --wraps du --description 'alias du1=du -h --max-depth 1 $argv | sort -h'
-  du -h --max-depth 1 $argv | sort -h
+
+function du1 --wraps du --description 'alias du1=du -hd1 $argv | sort -h'
+  switch $os
+  case Linux
+    du -h --max-depth 1 $argv | sort -h
+  case Darwin #macOS style
+    du -hd1 $argv | sort -h
+  end
 end
+
 function plantuml --description 'execute plantuml $argv (installed on vscode)'
-  java -jar ~/.var/app/com.visualstudio.code/data/vscode/extensions/jebbs.plantuml-2.12.1/plantuml.jar $argv
+  java -jar $HOME/.vscode/extensions/jebbs.plantuml-2.12.2/plantuml.jar $argv
 end
