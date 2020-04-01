577dcd511b62fe074c251d440d37e32894e3f5c2
nicolas Trauwaen <ntrauwaen@gmail.com>
2020-04-01 12:05:17 +0200

    maj version plantuml & ajout alias pour nvim

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -2,6 +2,10 @@
 #aliases
 set os (uname)
 
+function vi --wraps nvim --description 'alias vi=nvim'
+  nvim $argv
+end
+
 function difu --wraps diff --description 'alias difu=diff -u --color'
   diff -u --color $argv
 end
@@ -16,5 +20,5 @@ function du1 --wraps du --description 'alias du1=du -hd1 $argv | sort -h'
 end
 
 function plantuml --description 'execute plantuml $argv (installed on vscode)'
-  java -jar $HOME/.vscode/extensions/jebbs.plantuml-2.12.2/plantuml.jar $argv
+  java -jar $HOME/.vscode/extensions/jebbs.plantuml-2.13.6/plantuml.jar $argv
 end
