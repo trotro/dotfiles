1dfb0b2e2fccfe842ae280bb9c436f34b27beb16
nicolas T <trotro@users.noreply.github.com>
2025-04-14 10:37:37 +0200

    Config update

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    eval "$(/Users/n.trauwaen/.local/bin/mise activate fish)"
+    /opt/homebrew/bin/brew shellenv | source
+    mise activate fish | source
     starship init fish | source
+else
+    mise activate fish --shims | source
 end
