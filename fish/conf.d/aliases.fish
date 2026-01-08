a8e613a990aaefbc146ba6ad065f44e4d61e0c40
nicolas <ntrauwaen@gmail.com>
2026-01-08 11:03:09 +0100

    GitButler Integration Commit

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -11,9 +11,9 @@ set os (uname)
 #  end
 #end
 
-# function vi --wraps nvim --description 'alias vi=nvim'
-#   nvim $argv
-# end
+function vi --wraps nvim --description 'alias vi=nvim'
+  nvim $argv
+end
 
 function difu --wraps diff --description 'alias difu=diff -u --color'
   diff -u --color $argv
@@ -23,15 +23,15 @@ function ls --wraps lsd --description 'alias ls=lsd'
   lsd $argv
 end
 
+function lt --wraps lsd --description 'alias lt=lsd --tree'
+  lsd --tree $argv
+end
+
 # function plantuml --description 'execute plantuml $argv (installed on vscode)'
 #   java -jar $HOME/.vscode/extensions/jebbs.plantuml-2.13.6/plantuml.jar $argv
 # end
 
-function docker --wraps podman --description 'alias docker=podman'
-  podman $argv
-end
-
-#function dock1 --description 'ssh to dock1 server'
-#  ssh trotro@dock1.trauwaen.net -p36210 -i .ssh/id_rsa_croesus
+#function docker --wraps podman --description 'alias docker=podman'
+#  podman $argv
 #end
 
